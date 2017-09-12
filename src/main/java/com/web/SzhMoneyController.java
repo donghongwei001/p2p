fea29fc2c563e85f42991616cf27d4eb027a6bab
Haohao <1604038681@qq.com>
2017-09-12 08:51:20 +0800

    "szh 2017-9-12"

--- src/main/java/com/web/SzhMoneyController.java
+++ src/main/java/com/web/SzhMoneyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.entity.SzhMoney;
+import com.entity.SzhProject;
 import com.service.SzhMoneyService;
 
 @Controller
@@ -17,8 +18,25 @@ public class SzhMoneyController {
 	@RequestMapping(value="/queryMoney")
 	@ResponseBody
 	public List<SzhMoney> query(){
-		@SuppressWarnings("unchecked")
 		List<SzhMoney> sm=sms.queryMoney();
 		return sm;
+	}
+	@RequestMapping(value="/queryMoneyName")
+	@ResponseBody
+	public List<SzhMoney> queryMoneyName(String name){
+		List<SzhMoney> smm = sms.queryMoneyName(name);
+		return smm;
+	}
+	@RequestMapping(value="/queryMoneyProject")
+	@ResponseBody
+	public List<SzhMoney> queryMoneyProject(String moneyProject){
+		List<SzhMoney> smmm = sms.queryMoneyProject(moneyProject);
+		return smmm;
+	}
+	@RequestMapping(value="/queryMoneyTime")
+	@ResponseBody
+	public List<SzhMoney> queryMoneyTime(String ti,String me){
+		List<SzhMoney> smmmm = sms.queryMoneyTime(ti, me);
+		return smmmm;
 	}
 }
--- src/main/java/com/web/SzhProjectController.java
+++ src/main/java/com/web/SzhProjectController.java
@@ -1,6 +1,11 @@
 package com.web;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,17 +22,27 @@ public class SzhProjectController {
 	@RequestMapping(value="/queryProject")
 	@ResponseBody
 	public List<SzhProject> query(){
-		@SuppressWarnings("unchecked")
 		List<SzhProject> sp=sps.queryProject();
 		return sp;
 	}
-	/*@RequestMapping(value="/queryPart")
+	@RequestMapping(value="/queryPart")
 	@ResponseBody
-	public PagingResult findById(){			
-		PagingResult<JYPuserQuery> pResult = new PagingResult<JYPuserQuery>();
-		pResult.setRows(list1);
-		pResult.setTotal(list.size());
-		return pResult;
-	}*/
-
+	public List<SzhProject> queryPart(String comname){
+		List<SzhProject> spp = sps.queryPart(comname);
+		return spp;
+	}
+	@RequestMapping(value="/queryUser")
+	@ResponseBody
+	public List<SzhProject> queryUser(String user){
+		System.out.println(user);
+		List<SzhProject> sppp = sps.queryUser(user);
+		return sppp;
+	}
+	@RequestMapping(value="/queryTime")
+	@ResponseBody
+	public List<SzhProject> queryTime(String zzz,String hhh){
+		@SuppressWarnings("unchecked")
+		List<SzhProject> spppp = sps.queryTime(zzz, hhh);
+		return spppp;
+	}
 }
